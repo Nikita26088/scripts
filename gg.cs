0e383bbc46cf1b16d501858ac084317f01c2830c
Nikita26088 <120301928+Nikita26088@users.noreply.github.com>
2022-12-11 13:21:07 +0500

    Add files via upload

---
+++ gg.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class  gg : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //это просто коментарий не обрашай внимания)
+        print("Hello, World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
